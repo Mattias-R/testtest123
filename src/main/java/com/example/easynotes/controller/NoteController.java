c1366ac08eee17c9194dd1a6c10b52891693f7f4
Mattias-R <mattias.r@gmx.at>
2020-01-08 09:45:09 +0100

    Hallo

--- src/main/java/com/example/easynotes/controller/NoteController.java
+++ src/main/java/com/example/easynotes/controller/NoteController.java
@@ -18,12 +18,52 @@ public class NoteController {
     NoteRepository noteRepository;
 
     // Get All Notes
+// Get All Notes
+    @GetMapping("/notes")
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
+    }
+
 
     // Create a new Note
-
+// Create a new Note
+    @PostMapping("/notes")
+    public Note createNote( @Valid @RequestBody Note note) {
+        return noteRepository.save (note);
+    }
     // Get a Single Note
 
+    // Get a Single Note
+    @GetMapping("/notes/{id}")
+    public Note getNoteById( @PathVariable(value = "id") Long noteId) {
+        return noteRepository.findById (noteId)
+                .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+    }
     // Update a Note
+// Update a Note
+    @PutMapping("/notes/{id}")
+    public Note updateNote( @PathVariable(value = "id") Long noteId,
+                            @Valid @RequestBody Note noteDetails) {
+
+        Note note = noteRepository.findById(noteId)
+                .orElseThrow(() -> new ResourceNotFoundException( "Note", "id", noteId));
+
+        note.setTitle(noteDetails.getTitle());
+        note.setContent(noteDetails.getContent());
+
+        Note updatedNote = noteRepository.save(note);
+        return updatedNote;
+    }
+    // Delete a Note
 
     // Delete a Note
+    @DeleteMapping("/notes/{id}")
+    public ResponseEntity<?> deleteNote( @PathVariable(value = "id") Long noteId) {
+        Note note = noteRepository.findById(noteId)
+                .orElseThrow(() -> new ResourceNotFoundException( "Note", "id", noteId));
+
+        noteRepository.delete(note);
+
+        return  ResponseEntity.ok().build();
+    }
 }
